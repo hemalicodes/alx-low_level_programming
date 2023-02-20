c1b4b192ed975ac8babade80d586a00db93ff4b7
hemalicodes <worksobitd@gmailcom>
2023-02-20 01:21:08 -0800

    upper and lower case

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,24 +2,24 @@
 /**
  *  main  - Entry point
  *
- * Description: The program prints the alphabet in lowercase, and then in uppercase, followed by a new line.
+ * Description: prints the alphabet in lowercase and uppercase.
  *
  * Return: 0 Always
  */
 
 int main(void)
 {
-  int i;
-  int j;
+int i;
+int j;
 
 for (i = 'a'; i <= 'z'; i++)
 {
-  putchar(i);
+putchar(i);
 }
 
 for (j = 'A'; j <= 'Z'; j++)
 {
-  putchar(j);
+putchar(j);
 }
 putchar('\n');
 return (0);
