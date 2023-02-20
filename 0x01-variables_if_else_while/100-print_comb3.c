581631da47d399e223ca4340228259ca5e185f54
hemalicodes <worksobitd@gmailcom>
2023-02-20 07:27:39 -0800

    100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,12 +14,20 @@ for (i = 0; i < 9; i++)
 {
 for (j = i + 1; j < 10; j++)
 {
+  
 putchar( i + '0');
-putchar ( j + '0');  
+putchar ( j + '0');
+ 
+if (digit1 == 8 && digit2 == 9)
+continue;
+ 
 putchar (',');
 putchar (' ');
+ 
 }
 }
+ 
 putchar('\n');
 return (0);
+
 }
