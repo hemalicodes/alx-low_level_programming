967e455d3e0e22472048878ffa6ad7e372680f1b
hemalicodes <worksobitd@gmailcom>
2023-02-19 21:56:22 -0800

    Solve style problems inorder to pass linter

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,9 +2,12 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main: Entry point
+ * main - Entry point
  *
- more headers goes there */
+ * Description: 'the program assigns a random number to the variable n each time it is executed. Complete the source code in order to print whether the number stored in the variable n is positive or negative.'
+ *
+ *Return: Always 0 (Success)
+ */
 
 
 /* betty style doc for function main goes there */
