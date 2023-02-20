5da993b120f64d25580626afc00728b91f1817a9
hemalicodes <worksobitd@gmailcom>
2023-02-20 02:58:50 -0800

    print alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -13,11 +13,9 @@ int main(void)
 
   for(i = 'a'; i <= 'z'; i++)
   {
-    while(i != 'e' && i != 'q')
-      {
-	putchar(i);
-        i++;
-      }
+    if(i != 'e' && i != 'q')
+      
+	putchar(i);      
   }
 
   putchar('\n');
