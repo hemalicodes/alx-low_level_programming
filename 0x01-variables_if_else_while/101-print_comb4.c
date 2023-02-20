7bd3d03cfbda5269807f1278f23bc34395340014
hemalicodes <worksobitd@gmailcom>
2023-02-20 08:11:36 -0800

    101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,11 +11,11 @@ int main(void)
 {
   int i,j,k;
 
-  for (i = 0; i < 7; i++)
+  for (i = 0; i < 8; i++)
     {
-      for (j= i +1; j < 8; j++)
+      for (j= i +1; j < 9; j++)
 	{
-	  for (k = k + 1; k < 9; k++)
+	  for (k = k + 1; k < 10; k++)
 	    {
 	      putchar(i + '0');
 	      putchar(j + '0');
