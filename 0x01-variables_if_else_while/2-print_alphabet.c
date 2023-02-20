13b2d8179780b25ea6d77728b696d4cb848828f8
hemalicodes <worksobitd@gmailcom>
2023-02-20 00:41:59 -0800

    lowercase the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description : This program will print the last aphabet in lowercase
+ *
+ * Return: 0 Always.
+ */
+
+int main(void)
+{
+  int i;
+
+  for (i = 'a';i <= 'z'; i++)
+    {
+      putchar(i);
+      putchar('\n');
+}
+  return 0;
+
+}
