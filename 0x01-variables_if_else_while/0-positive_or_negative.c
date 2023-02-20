5576d21b372dec42c54db2bd1d7f97e6517ffee9
hemalicodes <worksobitd@gmailcom>
2023-02-19 22:12:34 -0800

    Solve style problems inorder to pass linter

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,7 +9,6 @@
  *Return: Always 0 (Success)
  */
 
-
 /* betty style doc for function main goes there */
 int main(void)
 {
@@ -20,15 +19,15 @@ int main(void)
 	/* your code goes there */
 	if (n > 0)
 	{
-	  printf("%d is positive\n", n);
+	  printf("%d is positive \n", n);
 	}
 	else if (n == 0)
 	{
-	  printf("%d is zero\n", n);
+	  printf("%d is zero \n", n);
 	}
 	else
 	{
-	  printf("%d is negative\n", n);
+	  printf("%d is negative \n", n);
 	}
 	return (0);
 }
