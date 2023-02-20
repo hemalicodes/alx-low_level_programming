6f069837a17e4f74054644e8c5e6ba9e55d7bf01
hemalicodes <worksobitd@gmailcom>
2023-02-20 03:41:31 -0800

    printing numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: program that prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+  int i;
+
+  for (i = 0; i < 10; i++)
+    {
+      printf(i);
+      printf('\n');
+
+    }
+  return (0);
+
+}
