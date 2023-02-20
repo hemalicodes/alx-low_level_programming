9d679d4496720517a8721cb28de20bba1c144d02
hemalicodes <worksobitd@gmailcom>
2023-02-20 04:04:02 -0800

    using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description:  prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+  int i;
+
+  for(i=0; i < 10; i++)
+    {
+      putchar( %d, i);
+    }
+  putchar('\n');
+  return (0);
+}
