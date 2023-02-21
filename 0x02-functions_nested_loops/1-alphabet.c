57dae06994ece5e08c842390a1944a2b30abfecc
hemalicodes <worksobitd@gmailcom>
2023-02-21 14:48:46 -0800

    prints the alphabet, in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints the alphabet, in lowercase
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+int i;
+for (i = 'a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+}
