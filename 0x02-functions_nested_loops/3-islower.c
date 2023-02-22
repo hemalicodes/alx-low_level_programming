724063b51488dc60aa15c7f5bf3bd3e914d672a5
hemalicodes <worksobitd@gmailcom>
2023-02-22 00:09:09 -0800

    checks for lowercase character.

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * int _islower(int c) -  checks for lowercase character
+ *
+ * Returns: 1 0r 0 
+ */
+
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
+}
