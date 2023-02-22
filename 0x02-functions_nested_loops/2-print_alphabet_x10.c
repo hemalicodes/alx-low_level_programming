5ea07b0a85103105c193eff9c9d9796d8710f463
hemalicodes <worksobitd@gmailcom>
2023-02-21 23:15:06 -0800

    prints the alphabet, in lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ *
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ *
+ */
+void print_alphabet_x10(void)
+{
+int i, counter;
+counter = 0;
+while (counter < 10)
+{
+for (i = 'a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+counter++;
+putchar('\n');
+}
+}
