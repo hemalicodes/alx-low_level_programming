0adf3614892bbcc29352f12dd44ca87f2d856d53
hemalicodes <worksobitd@gmailcom>
2023-02-22 06:08:51 -0800

    updates

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -9,16 +9,12 @@ int main(void)
 {
 int r;
 r = print_sign(98);
-putchar(',');
 putchar(r + '0');
 r = print_sign(0);
-putchar(',');
 putchar(r + '0');
 r = print_sign(0xff);
-putchar(',');
 putchar(r + '0');
 r = print_sign(-1);
-putchar(',');
 putchar(r + '0');
 return (0);
 }
