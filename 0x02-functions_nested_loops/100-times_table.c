1d5feee6c312cfe39e8fd5ef1c9a8848929a8f08
hemalicodes <worksobitd@gmailcom>
2023-02-22 09:46:19 -0800

    100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,6 +4,15 @@
  * @n: input
  * Return: Always 0 (Success)
  */
-void print_times_table()
+void print_times_table(int n)
 {
+ // Check if n is valid (between 0 and 15)
+if (n < 0 || n > 15) {
+return;
+}
+// Print the multiplication table
+for (int i = 0; i <= 10; i++)
+{
+printf("%d x %d = %d\n", i, n, i * n);
+}
 }
