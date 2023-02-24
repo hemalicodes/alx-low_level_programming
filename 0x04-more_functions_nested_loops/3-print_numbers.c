9b628aa3fe8e5112c104cef929613859ef16a701
hemalicodes <worksobitd@gmailcom>
2023-02-24 06:45:25 -0800

    update

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
  * print_numbers - prints the numbers from 0 to 9
  *
- * Return: Always 0
+ * Return: 0 - 9
  */
-
 void print_numbers(void)
 {
 for (int i = 0; i < 10; i++)
