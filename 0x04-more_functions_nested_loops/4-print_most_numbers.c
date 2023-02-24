875ad0cb47479da81390f12474a0f24c09885e94
hemalicodes <worksobitd@gmailcom>
2023-02-24 08:08:18 -0800

    prints the numbers, from 0 to 9

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,10 +11,10 @@ void print_most_numbers(void)
 int i;
 for (i = 0; i <= 9; i++)
 {
-if ((i == 2) || (i == 4))
-continue;
-else
+if (i != 2 && i != 4)
+{
 putchar(i + '0');
+}
 }
 putchar('\n');
 }
