b914a192326a92facf0d563e034d7b574ca57c0f
hemalicodes <worksobitd@gmailcom>
2023-02-24 06:29:26 -0800

    1-isdigit m

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ *
+ * @a: input
+ * @b: input
+ *
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+int result;
+result = a * b;
+return (result);
+}
