3a6b69c309313112fafeccbbde3c1e0f284c75f5
hemalicodes <worksobitd@gmailcom>
2023-02-24 10:54:57 -0800

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,5 +1,5 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
  * print_number - function to print integers
@@ -8,26 +8,12 @@
  */
 void print_number(int n)
 {
-unsigned int a, b, count, tmp, pow;
-a = n;
-pow = b = 1;
-if (n < 0)
-{
-a = a * -1;
+if (n < 0) {
 putchar('-');
+n = -n;
 }
-tmp = a;
-while (tmp > 9)
-{
-b++;
-tmp = tmp / 10;
+if (n / 10 != 0) {
+print_number(n / 10);
 }
-for (count = 1; count < b; count++)
-pow = pow * 10;
-while (pow > 1)
-{
-putchar((a / pow) % 10 + '0');
-pow = pow / 10;
-}
-putchar(a % 10 + '0');
+putchar(n % 10 + '0');
 }
