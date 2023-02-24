5e27cc6e85624568950e81ac1438b48cca91bf82
hemalicodes <worksobitd@gmailcom>
2023-02-24 07:55:44 -0800

    prints the numbers, from 0 to 9

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * print_most_numbers - Prints the numbers from 0-9 except for 2 and 4.
  */
@@ -9,7 +9,7 @@ int num;
 for (num = 0; num <= 9; num++)
 {
 if (num != 2 && num != 4)
-_putchar(num + '0');
+putchar(num + '0');
 }
-_putchar('\n');
+putchar('\n');
 }
