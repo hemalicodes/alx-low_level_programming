f543984f3d8baa37170b7753a812bd1a7d242f58
hemalicodes <worksobitd@gmailcom>
2023-02-28 13:01:57 -0800

    prints a string, in reverse, followed by a new line

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,17 +10,17 @@
 void print_rev(char *s)
 {
 int len = 0;
-while(*s != '\0')
+while (*s != '\0')
 {
 len++;
 s++;
 }
 s--;
-while(len > 0)
+while (len > 0)
 {
 putchar(*s);
 s--;
 len--;
 }
 putchar('\n');
-} 
+}
