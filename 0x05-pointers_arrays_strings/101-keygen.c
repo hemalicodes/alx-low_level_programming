afd3374e6944a8b481e561b9e9eb3e4f045350d1
hemalicodes <worksobitd@gmailcom>
2023-02-28 15:20:11 -0800

    Password Generator

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+char password[PASSWORD_LENGTH + 1];
+const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+srand(time(NULL));
+for (int i = 0; i < PASSWORD_LENGTH; i++)
+{
+password[i] = charset[rand() % (sizeof(charset) - 1)];
+}
+password[PASSWORD_LENGTH] = '\0';
+printf("%s\n", password);
+return 0;
+}
