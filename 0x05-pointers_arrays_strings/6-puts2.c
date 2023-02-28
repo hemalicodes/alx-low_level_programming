7fd69b1ee8f2dd8dd8b1c6e9975e9c7733adfee4
hemalicodes <worksobitd@gmailcom>
2023-02-28 13:48:50 -0800

    prints every other character of a string, starting with the first character, followed by a new line

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *puts2
+ *
+ *@str: input
+ *
+ *Return: Always 0
+ */
+void puts2(char *str)
+{
+int i;
+for (i = 0; i <= str; i++)
+{
+putchar(%d,i);
+}
+putchar('\n');
+} 
