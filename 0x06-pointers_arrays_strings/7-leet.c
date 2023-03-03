270779a36868510af7de8fe931a121bc10891408
sobitd <sobitdaniel@gmail.com>
2023-03-03 23:41:44 +0300

    seven

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *leet -  function that encodes a string into 1337
+ *@str: input
+ *Return: Always 0
+ */
+char *leet(char *)
+{
+int i, j;
+char c[] = "aAeEoOtTlL";
+char n[] = "4433007711";
+i = 0;
+while (str[i] != '\0')
+{
+j = 0;
+while (c[j] != '\0')
+{
+if (str[i] == c[j])
+{
+str[i] = n[j];
+}
+j++;
+}
+i++;
+}
+return (str);
+}
