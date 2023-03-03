bf02470f75dfa1219992b399dd4b6db269159b2d
sobitd <sobitdaniel@gmail.com>
2023-03-03 23:56:51 +0300

    101

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_number - function that prints an integer
+ *@n: input
+ *Return: Always 0
+ */
+void print_number(int n)
+{
+unsigned int i = n;
+if (n < 0)
+{
+putchar(45);
+i = -i;
+}
+if (i / 10)
+{
+print_number(i / 10);
+}
+putchar(i % 10 + '0');
+}
