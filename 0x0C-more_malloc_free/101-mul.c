ad2f8d094bc4a37f2dfa8f38142545004614aa02
sobitd <sobitdaniel@gmail.com>
2023-03-16 20:59:26 +0300

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,7 +1,6 @@
 #include "main.h"	
 #include <stdlib.h>		
-#include <stdio.h>	
-#define ERR_MSG "Error"		
+#include <stdio.h>
 /**		
  * is_digit - checks if a string contains a non-digit char		
  * @s: string to be evaluated		
