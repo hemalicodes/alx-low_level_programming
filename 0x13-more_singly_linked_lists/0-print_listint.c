6e9c7bb1701af98a057abcac62e081e2f1e37585
sobitd <sobitdaniel@gmail.com>
2023-04-06 00:46:27 +0300

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,9 +4,10 @@
 /**
  * print_listint - prints all the elements in a listint_list
  * @h: head of listint_t list pointer
+ *
  * Return: nodes in the list
  */
-size_t print_listint(const listint_t *h)
+size_t print_listint(const listint_t*h)
 {
 size_t num = 0;
 for (; h != Null; h = h->next)
